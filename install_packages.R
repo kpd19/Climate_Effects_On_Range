b2227637086b824ca8e596688fdf8579f89821ab
Katie Dixon <kpd19@case.edu>
2025-01-23 16:02:32 -0600

    Adding install packages script for R

---
+++ install_packages.R
@@ -0,0 +1,9 @@
+################################################
+## Install packages for the analysis 
+################################################
+
+## Install multiple packages
+install.packages(c("tidyverse","gridExtra","raster","sp",
+                   "geodata","dismo","deldir",'geonames',
+                   'randomForest','MESS'),
+                 dependencies = T)
