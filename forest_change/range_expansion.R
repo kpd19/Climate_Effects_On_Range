b9a13a716f99c939600a12d27dffd0d9093cde4b
Katie Dixon <kpd19@case.edu>
2025-03-05 17:30:49 -0600

    Fixing typo in writing csvs

--- forest_change/range_expansion.R
+++ forest_change/range_expansion.R
@@ -27,8 +27,12 @@ latlong %>% count(source)
 elev_info <- latlong %>% select(lat,lon,manual_id,source,elev2)
 
 species_class <- read_csv("../landscape/data/species_class.csv")
-forest_current <- read_csv("../landscape/data/forest_composition.csv")
 forest_presence <- read_csv("data/forest_climate_change2.csv")
+
+forest1 <- read_csv("../landscape/data/forest_composition.csv")
+forest2 <- read_csv("../landscape/data/forest_composition_new.csv")
+
+forest_current <- rbind(forest1,forest2)
 
 forest_presence %>% filter(tree_sp == "Abies grandis") %>% 
   ggplot() + aes(x = lon,y = lat, color = present) + geom_point() + theme_classic() + 
@@ -111,4 +115,77 @@ for(i in 1:length(uni_sp)){
   dev.off()
 }
 
+needleleaf <- forest_all2 %>% drop_na(elev2) %>%
+  mutate(lat_coord = round(lat/0.25)*0.25,
+         lon_coord = round(lon/0.25)*0.25) %>%
+  group_by(lat_coord,lon_coord,time_period) %>% 
+  summarize(sum_pres = sum(present)) %>% pivot_wider(values_from = sum_pres, names_from = time_period) %>% 
+  mutate(`2005` = ifelse(is.na(`2005`),0,`2005`),
+         `2050` = ifelse(is.na(`2050`),0,`2050`),
+         `2100` = ifelse(is.na(`2100`),0,`2100`))
 
+needleleaf <- needleleaf %>% mutate(exp_2050 = case_when(`2005` == 0 & `2050` >= 1 ~ 'expansion',
+                                                     `2005` >= 1 & `2050` == 0 ~ 'contraction',
+                                                     `2005` >= 1 & `2050` >= 1 ~ 'current range',
+                                                     `2005` == 0 & `2050` == 0 ~ 'absent')) %>% 
+  mutate(exp_2100 = case_when(`2005` == 0 & `2100` >= 1 ~ 'expansion',
+                              `2005` >= 1 & `2100` == 0 ~ 'contraction',
+                              `2005` >= 1 & `2100` >= 1 ~ 'current range',
+                              `2005` == 0 & `2100` == 0 ~ 'absent'))
+
+nl_long <- needleleaf %>% select(lat_coord,lon_coord,exp_2050, exp_2100) %>% pivot_longer(cols = c(exp_2050,exp_2100)) %>% 
+  mutate(period = ifelse(name == 'exp_2050', 2050,2100))
+
+nl_plt <- ggplot() +
+  geom_tile(data = nl_long,
+            aes(x = lon_coord, y= lat_coord, color = value, fill = value)) + theme_classic() + 
+  scale_color_manual("", values = c('expansion' = 'blue','contraction' = 'orange','current range' = 'green4','absent' = 'grey75')) +
+  scale_fill_manual("", values = c('expansion' = 'blue','contraction' = 'orange','current range' = 'green4','absent' = 'grey75')) +
+  xlab("Longitude") + ylab("Latitude") + 
+  facet_wrap(~period)
+
+plt_diff <- nl_plt + geom_sf(data = state_data, aes(geometry = geometry), color = "grey55", fill = NA, size = 1) +
+  geom_sf(data = bc_data, aes(geometry = geometry), color = "grey55", fill = NA, size = 1) +
+  theme_classic(base_size = 15) +
+  coord_sf(ylim = c(32,52), xlim = c(-127,-103)) +
+  guides(shape = guide_legend(override.aes = list(size = 0.25))) +
+  theme(plot.title = element_text(hjust = 0.5))+ 
+  theme(legend.position = 'top')
+plt_diff
+
+pdf(paste0("figures/host_tree_change.pdf"),height = 6, width = 8)
+print(plt_diff)
+dev.off()
+
+forest_current_wide <- forest_current_long2 %>% pivot_wider(values_from = present, names_from = tree_sp) %>% 
+  mutate(Abies = `Abies amabilis` + `Abies balsamea` + `Abies concolor` + `Abies grandis` + 
+           `Abies lasiocarpa` + `Abies magnifica` + `Abies procera`,
+         Pseudotsuga = `Pseudotsuga menziesii` + `Pseudotsuga macrocarpa`) %>% 
+  mutate(host_trees = Abies + Pseudotsuga)
+
+forest_current_wide <- forest_current_wide %>% mutate(Abies = ifelse(Abies >=1,1,0),
+                               Pseudotsuga= ifelse(Pseudotsuga >=1,1,0),
+                               host_trees = ifelse(host_trees >=1,1,0))
+
+forest_2050_wide <- forest_presence %>% filter(time_period == 2050) %>% pivot_wider(values_from = present, names_from = tree_sp) %>% 
+  mutate(Abies = `Abies amabilis` + `Abies balsamea` + `Abies concolor` + `Abies grandis` + 
+           `Abies lasiocarpa` + `Abies magnifica` + `Abies procera`,
+         Pseudotsuga = `Pseudotsuga menziesii` + `Pseudotsuga macrocarpa`) %>% 
+  mutate(host_trees = Abies + Pseudotsuga) %>% mutate(Abies = ifelse(Abies >=1,1,0),
+                                                     Pseudotsuga= ifelse(Pseudotsuga >=1,1,0),
+                                                     host_trees = ifelse(host_trees >=1,1,0))
+
+forest_2100_wide <- forest_presence %>% filter(time_period == 2100) %>% pivot_wider(values_from = present, names_from = tree_sp) %>% 
+  mutate(Abies = `Abies amabilis` + `Abies balsamea` + `Abies concolor` + `Abies grandis` + 
+           `Abies lasiocarpa` + `Abies magnifica` + `Abies procera`,
+         Pseudotsuga = `Pseudotsuga menziesii` + `Pseudotsuga macrocarpa`) %>% 
+  mutate(host_trees = Abies + Pseudotsuga) %>% mutate(Abies = ifelse(Abies >=1,1,0),
+                                                      Pseudotsuga= ifelse(Pseudotsuga >=1,1,0),
+                                                      host_trees = ifelse(host_trees >=1,1,0))
+
+forest_2100_wide %>% ggplot() + aes(x = lon, y = lat, color = host_trees) + geom_point() + 
+  theme_classic()
+
+write_csv(forest_current_wide,"data/forest_composition_2005.csv")
+write_csv(forest_2050_wide,"data/forest_composition_2050.csv")
+write_csv(forest_2100_wide,"data/forest_composition_2100.csv")
